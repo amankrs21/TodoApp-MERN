1f621ac3aa31d32f8b9c49acbda85540f8864eb0
Aman Kumar Singh <amansingh844123@gmail.com>
2024-09-28 02:23:18 +0530

    Update AuthUser.jsx (Backend API Call Corrected.)

--- react/src/components/AuthUser.jsx
+++ react/src/components/AuthUser.jsx
@@ -5,8 +5,8 @@ import { useNavigate } from "react-router-dom";
 
 // Create an instance of Axios with default configurations
 const http = axios.create({
-    baseURL: "http://localhost:3000/",
-    // baseURL: "https://todomern-64mu.onrender.com/",
+    // baseURL: "http://localhost:3000/",
+    baseURL: "https://todomern-64mu.onrender.com/",
     headers: {
         "Content-type": "application/json"
     },
